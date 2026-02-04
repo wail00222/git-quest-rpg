4c6a799076fe010aa7a6f5ee88f8fc752cbdbedd
YcnChB <yacinecheurfi00@gmail.com>
2026-02-04 15:10:39 +0100

    fixing class zombie

--- Zombie.py
+++ Zombie.py
@@ -1,10 +1,4 @@
-
 class Zombie(Enemy):
-    def __init__(self, name="Zombie", health=50, attack_power=10):
-        super().__init__(name, health, attack_power)
 
-    def attack(self, target):
-        damage = self.attack_power
-        target.health -= damage
-        print(f"{self.name} bites {target.name} for {damage} damage!")
-    
+    def attack(self):
+        return f"{self.name} bites and deals {self.degat} damage!"
