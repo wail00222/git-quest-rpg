79ddb1b76415749135802f4f29820c46bdc6cc52
YcnChB <yacinecheurfi00@gmail.com>
2026-02-04 15:33:13 +0100

    coding the class Orc closes #4

--- Orc.py
+++ Orc.py
@@ -1,8 +1,11 @@
+import random
+from Enemy import Enemy
+
 class Orc(Enemy):
 
     def __init__(self):
         super().__init__("Orc")
-        # Définition des attributs spécifiques à l'Orc
+       
         self.degat = random.choice([3, 4, 5])
         self.loot = random.choice([2, 2.5])
 
